a0e191f48997c1698efb240470d1b97716d0b051
Emre Guneyler <eguneys@hotmail.com>
2021-01-05 21:51:37 +0000

    initial commit

---
+++ setup.sh
@@ -0,0 +1,9 @@
+# Using Ubuntu
+curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt update && sudo apt install yarn
